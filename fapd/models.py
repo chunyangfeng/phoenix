c1853edc2bf4d67e5906a06c7459c95cb596de68
fengchunyang <fengcy@vastdata.com.cn>
2021-03-02 19:15:43 +0800

    fix: 项目流程控制models调整

--- fapd/models.py
+++ fapd/models.py
@@ -25,6 +25,7 @@ class ProjectInCharge(BasicModel):
     identity = models.CharField(verbose_name="唯一性证明", max_length=255, unique=True)
     phone = models.CharField(verbose_name="联系方式", max_length=16, blank=True, null=True)
     email = models.CharField(verbose_name="电子邮箱", max_length=255, blank=True, null=True)
+    remark = models.CharField(verbose_name="备注说明", max_length=255, blank=True, null=True)
 
     class Meta:
         db_table = 'fapd_project_in_charge'
@@ -34,14 +35,14 @@ class ProjectInCharge(BasicModel):
 class ProjectInfo(CommonDataModel):
     """项目/业务信息记录表"""
     project = models.ForeignKey('ProjectInfo', on_delete=models.CASCADE, related_name='sub_project', blank=True,
-                                null=True, help_text='自关联，如果关联有外键，则为子项目')
+                                null=True, help_text='自关联，如果关联有外键，则当前实例为子项目')
     name = models.CharField(verbose_name="项目名称", max_length=255, unique=True)
     in_charge = models.ManyToManyField('ProjectInCharge', verbose_name="责任人")
     stime = models.DateTimeField(verbose_name="项目开始时间")
     etime = models.DateTimeField(verbose_name="项目结束时间")
     dtime = models.DateTimeField(verbose_name="项目完成时间", blank=True, null=True)
     budget = models.BigIntegerField(verbose_name="项目预算", default=0)
-    status = models.CharField(verbose_name="项目状态", choices=params.PROJECT_STATUS_CHOICE,
+    status = models.CharField(verbose_name="项目状态", choices=params.PROJECT_STATUS_CHOICE, max_length=16,
                               default=params.PROJECT_STATUS_START)
 
     class Meta:
@@ -54,7 +55,7 @@ class ProjectPlanTask(CommonDataModel):
     project = models.ForeignKey('ProjectInfo', verbose_name="关联项目", on_delete=models.CASCADE,
                                 related_name='plan_task', help_text='关联项目')
     name = models.CharField(verbose_name="任务名称", max_length=255)
-    status = models.CharField(verbose_name="任务状态", choices=params.TASK_STATUS_CHOICE,
+    status = models.CharField(verbose_name="任务状态", choices=params.TASK_STATUS_CHOICE, max_length=16,
                               default=params.TASK_STATUS_PLAN, help_text="任务当前的状态")
     dtime = models.DateTimeField(verbose_name="任务完成时间", blank=True, null=True)
     in_charge = models.ManyToManyField('ProjectInCharge', verbose_name="责任人")
